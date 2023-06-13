4e18598a5bad1179e6fe03d9295581d428ea2bb2
Konstantin <alkapone10031993@gmail.com>
2023-06-13 20:41:10 +0300

    1finish

--- Seminar1/1/Program.cs
+++ Seminar1/1/Program.cs
@@ -10,4 +10,6 @@
 Console.Write("Input number => ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int squareNumber
+int power = 2; // power of number
+int squareNumber = (int)Math.Pow (number, power);
+Console.WriteLine($"Square {number} is {squareNumber}");
