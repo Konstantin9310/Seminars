30158484318371a869563563baf5c85bd010da2b
Konstantin <alkapone10031993@gmail.com>
2023-06-16 16:06:06 +0300

    4

---
+++ Seminar2/4/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Напишите программу, которая принимает на вход число и п
+роверяет, кратно ли оно одновременно 
+7 и 23.
+
+14 -> нет 
+46 -> нет 
+161 -> да
+*/
+
+void GetNumber()
+{
+    Console.Write(" Введите число ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    if(num%7==0 && num%23 ==0)
+    {
+        Console.Write(" кратное ");
+    }
+    else
+    {
+        Console.WriteLine(" не кратное ");
+    }
+
+}
+GetNumber();
