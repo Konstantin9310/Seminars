dd9483dbe8216c52bc9c31890257e957e131a8ac
Konstantin <alkapone10031993@gmail.com>
2023-06-19 14:02:52 +0300

    new

---
+++ Seminar3/1/Program.cs
@@ -0,0 +1,57 @@
+﻿string data = "data";
+string error = "error";
+string result = "result";
+
+void Print(string text, string arg){
+    switch (arg)
+    {
+    case "data": Console.ForegroundColor = ConsoleColor.Yellow;    
+    break;       
+    
+    case "result": Console.ForegroundColor = ConsoleColor.Green;    
+    break;   
+    case "error": Console.ForegroundColor = ConsoleColor.Red;    
+    break;   
+
+    }
+    Console.WriteLine(text);
+    Console.ResetColor();
+}
+
+int GetItNum(){
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void FindQuarter(int x, int y){
+    if(x == 0 || y == 0){
+        Print("Our or bouth coordinate equal 0", error);
+    }
+    else {
+        switch ((x, y))
+        {
+            case (>0, >0): Print ("First quarter.", result);
+            break;
+            
+            case (<0, >0): Print ("Second quarter.", result);
+            break;
+
+            case (<0, <0): Print ("Third quarter.", result);
+            break;
+
+            case (>0, <0): Print ("Fourth quarter.", result);
+            break;
+
+        }
+    }
+}
+    
+Print("Input X coordinate", data);
+int coordinateX = GetItNum();
+
+Print("Input Y coordinate", data);
+int coordinateY = GetItNum();
+
+//Print($"Our Coordinate X = {coordinateX}, Y = {coordinateY} data");
+
+FindQuarter(coordinateX, coordinateY);
