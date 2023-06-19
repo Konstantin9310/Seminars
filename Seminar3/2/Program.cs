833ec0d74cb40142ab01a7416bb5e37ef0c42d10
Konstantin <alkapone10031993@gmail.com>
2023-06-19 14:37:51 +0300

    new2

---
+++ Seminar3/2/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+Напишите программу, которая по заданному номеру четверти,
+ показывает диапазон возможных координат точек в этой четверти (x и y).
+*/
+
+
+
+
+void Print(string text){
+    
+    Console.WriteLine(text);
+  
+}
+
+int GetItNumber(){
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void FindCoordinate(int number){
+    if(number > 0 && number < 5){
+     
+        switch (number)
+        {
+            case 1: Print ("x > 0, y > 0");
+            break;
+            
+            case 2: Print ("x < 0, y > 0");
+            break;
+
+            case 3: Print ("x < 0, y < 0 ");
+            break;
+
+            case 4: Print ("x > 0, y < 0");
+            break;
+
+        }
+    }
+    else{
+        Print("insert correct number");
+    }
+}
+
+    
+Print("Input quarter");
+int number = GetItNumber();
+
+FindCoordinate(number);
