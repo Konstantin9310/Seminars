05da22c73b25bcc0517135a8b44123b5c9ee6290
Konstantin <alkapone10031993@gmail.com>
2023-07-10 10:11:09 +0300

    9

---
+++ Seminar9/4/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 65: Задайте значения M и N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5 -> "1, 2, 3, 4, 5"
+M = 4; N = 8 -> "4, 5, 6, 7, 8"
+*/
+
+Console.WriteLine("Input first number: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input second number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+void Print(int m, int n){
+    Console.WriteLine(m);
+    if(m < n) Print(m+1, n);
+}
+Print(m, n);
---
+++ Seminar9/5/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 69: Напишите программу, которая на вход принимает два 
+числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8
+*/
+
+
+Console.WriteLine("Input first number: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input second number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+int Print(int m, int n){
+    if(n == 0) return 1;
+    return m * Print(m, n-1);
+}
+Console.WriteLine(Print(m, n));
